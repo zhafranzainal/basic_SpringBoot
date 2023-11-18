c31a31de78d1428bbc01ae936ca576355299a297
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-18 11:10:19 +0800

    Create model Book

---
+++ src/main/java/com/example/librarymanagementsystem/models/Book.java
@@ -0,0 +1,36 @@
+package com.example.librarymanagementsystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
