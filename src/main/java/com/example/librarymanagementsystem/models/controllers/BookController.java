8f46948bf15aa7517897d43ab8a912b84ce527d6
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-18 11:44:37 +0800

    Create BookController.java

---
+++ src/main/java/com/example/librarymanagementsystem/models/controllers/BookController.java
@@ -0,0 +1,28 @@
+package com.example.librarymanagementsystem.models.controllers;
+
+import com.example.librarymanagementsystem.models.Book;
+import com.example.librarymanagementsystem.models.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+}
