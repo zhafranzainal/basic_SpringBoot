8304f3036f374a333e608922b3a0832b8a09e6fb
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-18 11:19:58 +0800

    Create BookService.java

---
+++ src/main/java/com/example/librarymanagementsystem/models/services/BookService.java
@@ -0,0 +1,24 @@
+package com.example.librarymanagementsystem.models.services;
+
+import com.example.librarymanagementsystem.models.Book;
+import com.example.librarymanagementsystem.models.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+}
